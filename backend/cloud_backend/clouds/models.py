4d472a95e8b6bff6034a9e2042c960b6cadf1a4b
hamzahsn <amdouni.hamza@hotmail.fr>
2020-11-08 22:18:55 +0100

    Create cloud model

--- backend/cloud_backend/clouds/models.py
+++ backend/cloud_backend/clouds/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Cloud(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True, default="")
+    latitude = models.DecimalField(max_digits=20, decimal_places=14, default=0)
+    longitude = models.DecimalField(max_digits=20, decimal_places=14, default=0)
+    region = models.CharField(max_length=100)
+
+    class Meta:
+        indexes = [models.Index(fields=["name"])]
