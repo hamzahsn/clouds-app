4520b8228a87d18545ab9eb95238aa26592d0085
hamzahsn <amdouni.hamza@hotmail.fr>
2020-11-09 08:26:08 +0100

    fetching clouds from the backend

---
+++ frontend/src/containers/CloudList/CloudList.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import Clouds from '../../components/Clouds/Clouds'
+import { testData } from './testData'
+import './CloudList.css'
+
+
+const CloudList: React.FC = () => {
+    const [clouds, setClouds] = useState([])
+
+    const getClouds = async () => {
+        const clouds = await fetch('http://127.0.0.1:8000/api/v1/clouds', { method: 'GET' })
+        const cloud = await clouds.json()
+        setClouds(cloud)
+    }
+
+    React.useEffect(() => {
+        getClouds()
+    }, [])
+
+    return (
+        <div className="App">
+            <Clouds {...clouds} />
+        </div>
+    )
+}
+
+export default CloudList
