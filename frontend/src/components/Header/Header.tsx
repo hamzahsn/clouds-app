d4ff62923a78d4359c20525c29b146484b522107
hamzahsn <amdouni.hamza@hotmail.fr>
2020-11-09 08:25:15 +0100

    Creating header component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className="navbar">
+            <div>
+                <p>Aiven Clouds</p>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
