090d26aa91a482a49d080ad50a3baf3ff79d398e
hamzahsn <amdouni.hamza@hotmail.fr>
2020-11-09 08:26:57 +0100

    consuming components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import CloudList from "./containers/CloudList/CloudList";
+import Header from "./components/Header/Header";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <CloudList />
     </div>
   );
 }
