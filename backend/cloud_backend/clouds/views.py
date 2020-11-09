bd2083de5ae3680587abf709e32c89d754d2226d
hamzahsn <amdouni.hamza@hotmail.fr>
2020-11-09 01:40:22 +0100

    Get list of clouds from Aiven API

--- backend/cloud_backend/clouds/views.py
+++ backend/cloud_backend/clouds/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 
@@ -11,13 +13,5 @@ class CloudViewSet(viewsets.ViewSet):
     serializer_class = CloudSerializer
 
     def list(self, request, format=None):
-        content = (
-            {
-                "cloud_description": "Africa, South Africa - Amazon Web Services: Cape Town",
-                "cloud_name": "aws-af-south-1",
-                "geo_latitude": -33.92,
-                "geo_longitude": 18.42,
-                "geo_region": "africa",
-            },
-        )
-        return Response(content)
+        request = requests.get("https://api.aiven.io/v1/clouds")
+        return Response(request.json())
